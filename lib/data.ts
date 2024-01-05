b3f7323251098bfe29a5b973108c6292a6b13ca7
Juliana Cotabarren <julianacotabarren@gmail.com>
2024-01-05 11:57:49 -0300

    feat: add projects

--- lib/data.ts
+++ lib/data.ts
@@ -1,121 +1,121 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import adminproImg from "@/public/adminpro.png";
-import aroundTheWorldImg from "@/public/aroundTheWorld.png";
-import spotifyImg from "@/public/spotify.png";
-import marvelSearcherImg from "@/public/marvelSearcher.png";
-import foodOrderAppImg from "@/public/foodOrderApp.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import adminproImg from '@/public/adminpro.png';
+import aroundTheWorldImg from '@/public/aroundTheWorld.png';
+import spotifyImg from '@/public/spotify.png';
+import marvelSearcherImg from '@/public/marvelSearcher.png';
+import foodOrderAppImg from '@/public/foodOrderApp.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: 'Graduated bootcamp',
+    location: 'Miami, FL',
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: '2019',
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: 'Front-End Developer',
+    location: 'Orlando, FL',
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: '2019 - 2021',
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: 'Full-Stack Developer',
+    location: 'Houston, TX',
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: '2021 - present',
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "AdminPro",
+    title: 'Around The World',
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["Angular", "Express.js", "MongoDB", "NodeJS"],
-    imageUrl: adminproImg,
-  },
-  {
-    title: "Around The World",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      'Hotel application to manage bookings, cabins and users. Users can login, manage their account, add/edit/remove cabins or bookings. It has features like filtering, sorting and pagination.',
+    tags: ['React', 'Vite', 'JavaScript', 'Styled-Components', 'Supabase'],
     imageUrl: aroundTheWorldImg,
   },
   {
-    title: "Spotify Clone",
+    title: 'AdminPro',
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["Angular", "Next.js", "SQL", "Tailwind", "Framer"],
+      'Administrative platform to manage doctors, hospitals and users. Users can login with email or Google account, change their account settings, add/edit/remove doctors and hospitals.',
+    tags: ['MongoDB', 'Express.js', 'Angular', 'NodeJS', 'TypeScript'],
+    imageUrl: adminproImg,
+  },
+  {
+    title: 'Spotify Clone',
+    description:
+      'Spotify web application with a set of songs that can be played or searched. It has features like login with email, play/pause and player with progress bar.',
+    tags: ['MongoDB', 'Express.js', 'Angular', 'NodeJS', 'TypeScript'],
     imageUrl: spotifyImg,
   },
   {
-    title: "Marvel Searcher",
+    title: 'Marvel Searcher',
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      'Web application for quick search of Marvel heroes. It shows random heroes, a description of their comics and dark/light mode. Users can select their favourite heroes.',
+    tags: ['React', 'Vite', 'JavaScript', 'Styled-Components'],
     imageUrl: marvelSearcherImg,
   },
   {
-    title: "Food Order App",
+    title: 'Food Order App',
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      'A delivery web application to order food. Users can select different meals that are added to a shopping cart and fill a reactive form to the delivery.',
+    tags: ['React', 'JavaScript', 'CSS', 'Firebase'],
     imageUrl: foodOrderAppImg,
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Angular",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "Express",
-  "PostgreSQL",
-  "Postman",
-  "Framer Motion",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Angular',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'Prisma',
+  'MongoDB',
+  'Redux',
+  'Express',
+  'PostgreSQL',
+  'Postman',
+  'Framer Motion',
 ] as const;
